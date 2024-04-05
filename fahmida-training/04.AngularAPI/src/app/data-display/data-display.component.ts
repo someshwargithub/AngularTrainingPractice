84ae68631840f3013d1cb99478778770891a3f6c
fahmidakm99 <fanihahaanifa@gmail.com>
2024-04-06 01:02:43 +0530

    api practice

---
+++ fahmida-training/04.AngularAPI/src/app/data-display/data-display.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-display',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './data-display.component.html',
+  styleUrl: './data-display.component.css'
+})
+export class DataDisplayComponent implements OnInit {
+
+  httpClient = inject(HttpClient);
+  data: any[]=[];
+  error: any='';
+  ngOnInit(): void {
+    this.getData();
+  }
+  getData(){
+    this.httpClient
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe((data : any) => {
+      console.log(data);
+      this.data = data;
+    },
+  error => {
+    console.log(error);
+    this.error=error.message;
+
+  }
+  );
+  }
+}
